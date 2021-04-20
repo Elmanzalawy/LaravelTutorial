ba10ac7b0bac8250bebdc1311529002669a0dba3
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2021-04-20 03:14:53 +0200

    Added Pusher and Echo packages
    We use Pusher broadcast events, and Echo to subscribe to events on the frontend.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Laravel Broadcasting</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+    </head>
+    <body>
+        <h1>Laravel Broadcasting</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quas? Expedita adipisci debitis a illum dignissimos perferendis nisi amet exercitationem? Odit ipsam voluptates consectetur laboriosam. Fugit unde placeat ipsam quis.</p>
+    </body>
+</html>
